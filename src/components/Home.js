86c6218c09824666beed69fc080c8c2796e46e55
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2022-10-20 22:38:15 +0530

    added code

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='section'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-12 text-center'>
+                  <h3 className='main-heading'>Library system</h3>               
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
